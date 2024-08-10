b763cfd7d7277f13660c456e5c8ded7f91ffc7c0
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-08-10 19:02:29 +0545

    Created Dockerfile for a simple Nodejs Application

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     return res.json({message: "Hey, I am nodejs in a container"});
